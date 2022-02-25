8b5f64e4cd0b62742e688b7e24f58bbd32ee4c7d
Matthew Puryear <mpuryear99@gmail.com>
2022-02-25 16:02:47 -0500

    Begin SCSS conversion & add footer to index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './scss/index.scss';
 import App from './App';
+import Footer from './components/Footer';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
