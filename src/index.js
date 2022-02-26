a05285353586fb98885dc619a39061f2ebbbdfb0
Matthew Puryear <mpuryear99@gmail.com>
2022-02-25 21:09:35 -0500

    Add utk header content and scss.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/index.scss';
 import App from './App';
+import Header from './components/Header';
 import Footer from './components/Footer';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
     <Footer />
   </React.StrictMode>,
