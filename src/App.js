36cfe1a0706330764c9ec75aba0c08da0d87d6b7
tlovett2 <tlovett2@vols.utk.edu>
2022-03-03 22:19:49 -0500

    Removed react spam

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Scheduler />
-      <div className="App-header">
+      {/* <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </div>
+      </div> */}
     </div>
   );
 }
