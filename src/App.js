a75b75e69e934e5cccf8897032b5aae0be5cfb1d
tlovett2 <tlovett2@vols.utk.edu>
2022-03-03 21:50:20 -0500

    Got blank scheduler on page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Scheduler from './components/Scheduler';
 
 function App() {
   return (
     <div className="App">
+      <Scheduler />
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
