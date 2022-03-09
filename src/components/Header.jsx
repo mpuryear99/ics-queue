7c544b246c02ba9b01eaffe92ba9f1fef4f4e598
Matthew Puryear <mpuryear99@gmail.com>
2022-03-09 11:25:02 -0500

    Add nav menu to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import HeaderNavMenu from "./HeaderNavMenu";
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 
 const Header = () => {
+  const topMarginMQ = useMediaQuery('(min-width:576px)');
+  const menuBtnStyle = {
+    float: 'right',
+    marginRight: '1rem',
+    marginTop: (topMarginMQ ? "1.3rem" : "0")
+  }
 
   return(
     <div id="utk-header-segment">
@@ -17,14 +25,7 @@ const Header = () => {
           <h1 className="ut-title">
             <a className="killer-logo" href="http://www.utk.edu">The University of Tennessee, Knoxville</a>
           </h1>
-          <button type="button" className="btn btn-findpage">
-            <span className="sr-only">Toggle navigation</span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-              <path d="M4 22h-4v-4h4v4zm0-12h-4v4h4v-4zm0-8h-4v4h4v-4zm3 0v4h17v-4h-17zm0 12h17v-4h-17v4zm0 8h17v-4h-17v4z" />
-            </svg>
-            <br/>
-            Menu
-          </button>
+          <HeaderNavMenu btnStyle={menuBtnStyle}/>
           <h2 className="site-title parent-show">
             <a href="http://innovate.utk.edu" title="Innovation & Collaboration Studio" rel="home">Innovation &amp; Collaboration Studio</a>
             <br/> {/* br origionally first element of TCE link */}
