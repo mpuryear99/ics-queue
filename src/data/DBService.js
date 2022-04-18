b488df66370222a3ae4a56ee001ab2576cbc2eda
Matthew Puryear <mpuryear99@gmail.com>
2022-04-18 11:07:41 -0400

    API for machines and appts by id

--- src/data/DBService.js
+++ src/data/DBService.js
@@ -8,7 +8,58 @@ class DBService {
    */
   async getMachines() {
     try {
-      var res = await fetch('api/getmachines');
+      var res = await fetch('api/machines');
+      var data = await res.json();
+    } catch (e) {
+      console.error(e);
+      return undefined;
+    }
+    return data;
+  }
+
+
+  /**
+   * Get machine with cooresponding _id from database
+   *
+   * @return Machine with _id
+   */
+  async getMachineByID(id) {
+    try {
+      var res = await fetch(`api/machines/${id}`);
+      var data = await res.json();
+    } catch (e) {
+      console.error(e);
+      return undefined;
+    }
+    return data;
+  }
+
+
+  /**
+   * Read appointments list from database.
+   *
+   * @return list of appointment objects
+   */
+  async getAppointments() {
+    try {
+      var res = await fetch('api/appointments');
+      var data = await res.json();
+    } catch (e) {
+      console.error(e);
+      return undefined;
+    }
+    return data;
+  }
+  
+
+  /**
+   * Get appointment with cooresponding _id from database
+   *
+   * @return Appointment with _id
+   */
+  async getAppointmentByID(id) {
+    try {
+      var res = await fetch(`api/appointments/${id}`);
       var data = await res.json();
     } catch (e) {
       console.error(e);
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,23 +4,54 @@
 //  https://webpack.js.org/configuration/dev-server/#devserveronbeforesetupmiddleware
 //  https://create-react-app.dev/docs/proxying-api-requests-in-development#configuring-the-proxy-manually
 //  https://github.com/facebook/create-react-app/blob/67b48688081d8ee3562b8ac1bf6ae6d44112745a/packages/react-scripts/config/webpackDevServer.config.js#L112-L122
+//  https://expressjs.com/en/api.html#res
 
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const { randomUUID } = require('crypto');
 const machines_json = require("./data/dev/machines.json");
+const appointments_mock = [];
+
+function delay(ms=2500) {
+  return new Promise(r => setTimeout(r, 2500));
+}
 
 module.exports = function(app) {
   if (process.env.NODE_ENV === 'production') {
     throw new Error("Mock API running in production.");
   }
 
-  // Mock API calls
   if (process.env.REACT_APP_MOCK_API === 'true') {
-
-    app.get('/api/getmachines', async (req, res) => {
-      await new Promise(r => setTimeout(r, 2500));
+    
+    //#region Mock API Calls
+    
+    // machines
+    app.get('/api/machines', async (req, res) => {
+      await delay();
       res.json(machines_json);
     });
 
+    // machines/<id>
+    app.get('/api/machines/*', async (req, res) => {
+      await delay();
+      let _id = req.params[0];
+      res.json(machines_json.find(x => x._id === _id));
+    });
+
+
+    // appointments
+    app.get('/api/appointments', async (req, res) => {
+      await delay();
+      res.json(appointments_mock);
+    });
+
+    // appointments/<id>
+    app.get('/api/appointments/*', async (req, res) => {
+      await delay();
+      let _id = req.params[0];
+      res.json(appointments_mock.find(x => x._id === _id));
+    });
+
+    //#endregion
 
   // Create a middlewate proxy to route api calls to the backend
   } else {
