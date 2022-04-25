7d33699b02a3e46e99d774453269aa26ef3eeaf1
Matthew Puryear <mpuryear99@gmail.com>
2022-04-25 06:29:03 -0400

    Clean index.js; move router to App.js; Add blank home page

---
+++ src/components/pages/HomePage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from 'logo.svg';
+import 'App.css';
+
+const HomePage = () => {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <span>Home Page (TODO)</span>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
