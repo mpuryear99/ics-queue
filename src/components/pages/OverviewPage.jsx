154bc55012fc203c6569094043b9a62eed865d3e
Matthew Puryear <mpuryear99@gmail.com>
2022-05-05 18:39:54 -0400

    Overview page

--- src/components/pages/OverviewPage.jsx
+++ src/components/pages/OverviewPage.jsx
@@ -3,40 +3,52 @@ import React from "react";
 import Timeline from 'react-calendar-timeline';
 // make sure you include the timeline stylesheet or the timeline will not be styled
 import 'react-calendar-timeline/lib/Timeline.css';
-import '../../scss/components/timeline.scss';
-import DBService from "../../data/DBService";
-import moment from "moment";
-import genAppointments from '../../data/dev/randomAppointments';
+import 'scss/components/timeline.scss';
+import DBService from "data/DBService";
+import moment from "moment-timezone";
+import genAppointments from 'data/dev/randomAppointments';
 
-const test_items = genAppointments(18);
-let start = moment().startOf('day');
-let end = moment().endOf('day');
+const TZ_NY = "America/New_York";
 
-const Scheduler = () => {
+let start = moment().tz(TZ_NY).startOf('day');
+let end = moment().tz(TZ_NY).endOf('day');
+
+const Scheduler = (props) => {
 
   const [machineList, setMachineList] = React.useState([]);
   const [appointmentList, setAppointmentList] = React.useState([]);
 
-  React.useLayoutEffect(() => {
+  React.useEffect(() => {
+    let isSubscribed = true;
     (async () => {
       let ml = await DBService.getMachines();
-      setMachineList(ml);
+      if (isSubscribed)
+        setMachineList(ml);
       
-      let startAfter = moment().startOf("day");
+      let startAfter = moment().tz(TZ_NY).startOf("day");
       let startBefore = startAfter.clone().add(1, "days");
 
-      let al = await DBService.getAppointmentsByQuery({startBefore, startAfter});
-      al = [...al, ...test_items]
+      let al = await DBService.getAppointmentsByQuery({
+        startBefore: startBefore.unix(), 
+        startAfter: startAfter.unix()
+      });
 
-      setAppointmentList(al);
+      al.forEach(a => {
+        a.startTime = moment.unix(a.startTime).tz(TZ_NY);
+        a.endTime = moment.unix(a.endTime).tz(TZ_NY);
+      });
 
+      //al = [...al, ...genAppointments(18)];
+      if (isSubscribed)
+        setAppointmentList(al);
     })();
+    return () => isSubscribed = false;
   }, []);
+
+  console.log(props)
 
   return(    
     <Timeline
-      // groups={calendarGroups}
-      // items={calendarItems}
       groups={machineList}
       items={appointmentList}
       keys={{
@@ -52,14 +64,12 @@ const Scheduler = () => {
       }}
 
       //This combination of start and end times properly shows one full 24 hour day
-      visibleTimeStart={moment().endOf('day').valueOf()}
-      visibleTimeEnd={moment().endOf('day').add(24, 'hours').valueOf()}
-
+      visibleTimeStart={moment().tz(TZ_NY).startOf('day')}
+      visibleTimeEnd={moment().tz(TZ_NY).endOf('day')}
+      
       //disable zoom
       minZoom = {end-start}
       maxZoom = {end-start}
-
-
     />
   )
 };
